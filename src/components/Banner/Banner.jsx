43527db9c43624aacbb0b6763790c5b394f2f935
searchsakib <searchsakib@gmail.com>
2023-10-07 17:47:55 +0600

    cards section half-done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,10 +8,10 @@ const Banner = () => {
     >
       <div className="hero-content text-center text-neutral-content ">
         <div>
-          <h1 className="text-3xl lg:text-8xl font-bold text-white pb-3 font-serif">
+          <h1 className="text-3xl lg:text-8xl font-bold text-blue-50 pb-3 font-serif">
             Elevate Your Corporate Events
           </h1>
-          <h2 className="text-xs lg:text-2xl font-semibold text-white font-serif">
+          <h2 className="text-xs lg:text-2xl font-semibold text-blue-50 font-serif">
             Transforming Ideas into Remarkable Realities
           </h2>
         </div>
--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -1,7 +1,27 @@
-const ServiceCard = () => {
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
+const ServiceCard = ({ service }) => {
+  const { id, image, title, short_desc, price } = service || {};
+
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+  // I have to add this to div: data-aos="fade-up"
+
   return (
-    <div>
-      <h2>this is service</h2>
+    <div className="card w-96 bg-base-100 shadow-xl" data-aos="fade-up">
+      <figure>
+        <img src={image} alt="pics" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title"> {title} </h2>
+        <p> {short_desc} </p>
+        <div className="card-actions justify-center">
+          <button className="btn btn-primary">Details</button>
+        </div>
+      </div>
     </div>
   );
 };
