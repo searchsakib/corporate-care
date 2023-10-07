80e5a80f74c74f277be512e8c62ad937535d509d
searchsakib <searchsakib@gmail.com>
2023-10-07 11:41:24 +0600

    logo added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -83,7 +83,10 @@ const NavBar = () => {
     <div className="flex flex-col md:flex-row lg:flex-row  md:px-10 lg:px-10 xl:px-0 items-center justify-between mt-12 mb-5 max-w-[1320px] mx-auto">
       <div>
         <Link>
-          <img src="/images/logo.png" alt="" />
+          {/* <img src="/images/logo.png" alt="" /> */}
+          <h2 className="font-bold text-blue-800 text-2xl font-serif">
+            Corporate <span className="text-white bg-blue-800">Care</span>{' '}
+          </h2>
         </Link>
       </div>
       <div className="text-center mt-9 md:mt-0 lg:mt-0 ">
