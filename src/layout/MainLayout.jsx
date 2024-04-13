4b74ca5eaa37d3e80e1419b7a6b4c20f8c61922a
searchsakib <searchsakib@gmail.com>
2024-04-14 04:36:30 +0600

    changed the website zoom to 90% from MainLayout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar/NavBar';
 import Footer from '../components/Footer/Footer';
+import { useEffect } from 'react';
 
 const MainLayout = () => {
+
+  useEffect(()=>{
+    document.body.style.zoom="90%"
+  },[])
+
   return (
     <div>
       <div className="pt-12 pb-12">
